a7ef91b0f58a6373b8b0a60d283c4edae3400118
Nateram <pablitonaterita@gmail.com>
2024-04-14 15:16:29 +0200

    empiezo bd

---
+++ BookingDAO/src/main/java/es/unex/pi/model/favoritos.java
@@ -0,0 +1,27 @@
+package es.unex.pi.model;
+//categorias
+public class favoritos {
+
+	private long idp;
+	private long idu;
+
+	
+
+
+	public long getIdp() {
+		return idp;
+	}
+	
+	public void setIdp(long idp) {
+		this.idp = idp;
+	}
+	public long getIdu() {
+		return idu;
+	}
+	
+	public void setIdu(long idu) {
+		this.idu = idu;
+	}
+
+	
+}
